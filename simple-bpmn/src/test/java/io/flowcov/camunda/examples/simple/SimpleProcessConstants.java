220a0413af0ec3ddcb18f9d840e735e7203e19fd
Dominik Horn <dominik.horn93@gmail.com>
2020-08-15 18:23:33 +0200

    Init Repo

---
+++ simple-bpmn/src/test/java/io/flowcov/camunda/examples/simple/SimpleProcessConstants.java
@@ -0,0 +1,10 @@
+package io.flowcov.camunda.examples.simple;
+
+public interface SimpleProcessConstants {
+
+    String PROCESS_KEY = "simpleProcess";
+    String USER_TASK_DO_SOMETHING = "UserTask_doSomething";
+    String END_EVENT_FINISHED = "EndEvent_finished";
+    String END_EVENT_CANCELED = "EndEvent_canceld";
+
+}
