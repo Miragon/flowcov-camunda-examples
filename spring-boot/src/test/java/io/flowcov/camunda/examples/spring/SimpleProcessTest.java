02cd2e8620915b5218977d3220a7163400448368
Dominik Horn <dominik.horn93@gmail.com>
2020-08-25 16:15:07 +0200

    Add Spring Example

--- spring-boot/src/test/java/io/flowcov/camunda/examples/spring/SimpleProcessTest.java
+++ spring-boot/src/test/java/io/flowcov/camunda/examples/spring/SimpleProcessTest.java
@@ -1,28 +1,40 @@
-package io.flowcov.camunda.examples.simple;
+package io.flowcov.camunda.examples.spring;
 
 import io.flowcov.camunda.junit.rules.FlowCovProcessEngineRuleBuilder;
 import lombok.val;
+import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
-import org.camunda.bpm.engine.test.assertions.bpmn.AbstractAssertions;
-import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.PostConstruct;
 
 import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.assertThat;
 import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.runtimeService;
 
+@RunWith(SpringJUnit4ClassRunner.class)
 @Deployment(resources = "bpmn/simpleProcess.bpmn")
+@ContextConfiguration(classes = {InMemProcessEngineConfiguration.class})
 public class SimpleProcessTest {
+
+    @Autowired
+    private ProcessEngine processEngine;
 
     @Rule
     @ClassRule
-    public static ProcessEngineRule rule = FlowCovProcessEngineRuleBuilder.create().withDetailedCoverageLogging().build();
+    public static ProcessEngineRule rule;
 
-    @Before
+    @PostConstruct
     public void init() {
-        AbstractAssertions.init(rule.getProcessEngine());
+        if (rule == null) {
+            rule = FlowCovProcessEngineRuleBuilder.create(processEngine).handleClassCoverage(true).withDetailedCoverageLogging().build();
+        }
     }
 
     @Test
