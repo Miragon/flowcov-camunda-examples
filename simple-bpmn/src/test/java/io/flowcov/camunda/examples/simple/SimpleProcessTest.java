b544eaaaca9c7de6a6646b70e0e19ed0d959ece2
Dominik Horn <dominik.horn93@gmail.com>
2020-09-09 15:14:23 +0200

    Add DMN

--- simple-bpmn/src/test/java/io/flowcov/camunda/examples/simple/SimpleProcessTest.java
+++ simple-bpmn/src/test/java/io/flowcov/camunda/examples/simple/SimpleProcessTest.java
@@ -4,26 +4,18 @@ import io.flowcov.camunda.junit.rules.FlowCovProcessEngineRuleBuilder;
 import lombok.val;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
-import org.camunda.bpm.engine.test.assertions.bpmn.AbstractAssertions;
-import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 
-import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.assertThat;
-import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.runtimeService;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.*;
 
-@Deployment(resources = "bpmn/simpleProcess.bpmn")
+@Deployment(resources = {"bpmn/simpleProcess.bpmn", "dmn/simpleDmn.dmn"})
 public class SimpleProcessTest {
 
     @Rule
     @ClassRule
     public static ProcessEngineRule rule = FlowCovProcessEngineRuleBuilder.create().withDetailedCoverageLogging().build();
-
-    @Before
-    public void init() {
-        AbstractAssertions.init(rule.getProcessEngine());
-    }
 
     @Test
     public void test_deploy_process() {
@@ -35,6 +27,14 @@ public class SimpleProcessTest {
     public void test_start_process() {
         val instance = runtimeService().startProcessInstanceByKey(SimpleProcessConstants.PROCESS_KEY);
         assertThat(instance).isWaitingAt(SimpleProcessConstants.USER_TASK_DO_SOMETHING);
+
+    }
+
+    @Test
+    public void test_complete_tsk() {
+        val instance = runtimeService().startProcessInstanceByKey(SimpleProcessConstants.PROCESS_KEY);
+        assertThat(instance).isWaitingAt(SimpleProcessConstants.USER_TASK_DO_SOMETHING);
+        complete(task(), withVariables("testVar", "test"));
 
     }
 
