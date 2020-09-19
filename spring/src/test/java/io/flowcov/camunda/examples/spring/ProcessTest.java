ee916ede014df0f998e1cbf56a271a842d6b80f4
Dominik Horn <dominik.horn93@gmail.com>
2020-09-19 16:37:04 +0200

    Refactoring Tests

---
+++ spring/src/test/java/io/flowcov/camunda/examples/spring/ProcessTest.java
@@ -0,0 +1,126 @@
+package io.flowcov.camunda.examples.spring;
+
+import io.flowcov.camunda.junit.FlowCovProcessEngineRuleBuilder;
+import lombok.val;
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.test.Deployment;
+import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.PostConstruct;
+
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@Deployment(resources = {"bpmn/StartWithFlowCov.bpmn", "dmn/DecideOnUsage.dmn"})
+@ContextConfiguration(classes = {InMemProcessEngineConfiguration.class})
+public class ProcessTest {
+
+    @Autowired
+    private ProcessEngine processEngine;
+
+    @Rule
+    @ClassRule
+    public static ProcessEngineRule rule;
+
+
+    private static String PROCESS_KEY = "StartWithFlowCov";
+    private static String TASK_REGISTER = "Task_RegisterOnFlowCov";
+    private static String TASK_INSTALL = "Task_InstallOnPremise";
+    private static String TASK_CREATE_REPOSITORY = "Task_CreateRepository";
+    private static String TASK_START_USING = "Task_StartUsingFlowCov";
+    private static String TASK_DISCUSS_FEEDBACK = "Task_DiscussFeedback";
+
+    private static String EVENT_FLOWCOV_USED = "EndEvent_FlowCovIsUsed";
+    private static String EVENT_FEEDBACK_PROCESSED = "EndEvent_FeedbackProcessed";
+
+    private static String MESSAGE_FEEDBACK_RECEIVED = "feedbackReceived";
+
+    @PostConstruct
+    public void init() {
+        if (rule == null) {
+            rule = FlowCovProcessEngineRuleBuilder.create(processEngine).build();
+        }
+    }
+
+    @Test
+    public void deploy_process() {
+        // just deployment
+    }
+
+    @Test
+    public void start_process() {
+        val instance = runtimeService().startProcessInstanceByKey(
+                PROCESS_KEY,
+                withVariables("cloudAccess", true, "openSource", true));
+        assertThat(instance).isWaitingAt(TASK_REGISTER);
+    }
+
+    @Test
+    public void register_and_use() {
+        val instance = runtimeService().startProcessInstanceByKey(
+                PROCESS_KEY,
+                withVariables("cloudAccess", true, "openSource", true));
+
+        assertThat(instance).isWaitingAt(TASK_REGISTER);
+        complete(task());
+
+        assertThat(instance).isWaitingAt(TASK_CREATE_REPOSITORY);
+        complete(task());
+
+        assertThat(instance).isWaitingAt(TASK_START_USING);
+        complete(task());
+
+        assertThat(instance).isEnded().hasPassed(EVENT_FLOWCOV_USED);
+    }
+
+    @Test
+    public void install_and_use() {
+        val instance = runtimeService().startProcessInstanceByKey(
+                PROCESS_KEY,
+                withVariables("cloudAccess", false, "openSource", false));
+
+        assertThat(instance).isWaitingAt(TASK_INSTALL);
+        complete(task());
+
+        assertThat(instance).isWaitingAt(TASK_CREATE_REPOSITORY);
+        complete(task());
+
+        assertThat(instance).isWaitingAt(TASK_START_USING);
+        complete(task());
+
+        assertThat(instance).isEnded().hasPassed(EVENT_FLOWCOV_USED);
+    }
+
+    @Test
+    public void register_and_give_feedback() {
+        val instance = runtimeService().startProcessInstanceByKey(
+                PROCESS_KEY,
+                withVariables("cloudAccess", true, "openSource", true));
+
+        assertThat(instance).isWaitingAt(TASK_REGISTER);
+        complete(task());
+
+        assertThat(instance).isWaitingAt(TASK_CREATE_REPOSITORY);
+        complete(task());
+
+        assertThat(instance).isWaitingAt(TASK_START_USING);
+
+        runtimeService().correlateMessage(MESSAGE_FEEDBACK_RECEIVED);
+        assertThat(instance).isWaitingAt(TASK_DISCUSS_FEEDBACK, TASK_START_USING);
+        complete(task(TASK_DISCUSS_FEEDBACK));
+
+        assertThat(instance).hasPassed(EVENT_FEEDBACK_PROCESSED);
+
+        complete(task(TASK_START_USING));
+        assertThat(instance).isEnded().hasPassed(EVENT_FLOWCOV_USED);
+
+    }
+
+}
